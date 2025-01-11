13566f4eecd4607bae0076da5d9d0e30b2d2b13d
Taro Matsuzawa aka. btm <btm@tech.email.ne.jp>
2025-01-11 15:01:01 +0900

    #5 convert distribute.sh for arm64's homebrew, copying recipes until bison

---
+++ qgis_deps/recipes/libxml2/recipe.sh
@@ -0,0 +1,80 @@
+#!/bin/bash
+
+DESC_libxml2="GNOME XML library"
+
+# version of your package
+VERSION_libxml2=2.13.5
+LINK_libxml2=libxml2.2.dylib
+
+# dependencies of this recipe
+DEPS_libxml2=()
+
+# url of the package
+# TODO fix the URL
+URL_libxml2=https://download.gnome.org/sources/libxml2/2.13/libxml2-${VERSION_libxml2}.tar.xz
+
+# md5 of the package
+MD5_libxml2=0b919be8edff97ade9c946e1a83bdecd
+
+# default build path
+BUILD_libxml2=$BUILD_PATH/libxml2/$(get_directory $URL_libxml2)
+
+# default recipe path
+RECIPE_libxml2=$RECIPES_PATH/libxml2
+
+# function called for preparing source code if needed
+# (you can apply patch etc here.)
+function prebuild_libxml2() {
+    cd $BUILD_libxml2
+
+    # check marker
+    if [ -f .patched ]; then
+        return
+    fi
+
+    patch_configure_file configure
+
+    touch .patched
+}
+
+function shouldbuild_libxml2() {
+    # If lib is newer than the sourcecode skip build
+    if [ ${STAGE_PATH}/lib/$LINK_libxml2 -nt $BUILD_libxml2/.patched ]; then
+        DO_BUILD=0
+    fi
+}
+
+# function called to build the source code
+function build_libxml2() {
+    try rsync -a $BUILD_libxml2/ $BUILD_PATH/libxml2/build-$ARCH/
+    try cd $BUILD_PATH/libxml2/build-$ARCH
+    push_env
+
+    try ${CONFIGURE} \
+        --disable-debug \
+        --without-lzma \
+        --without-python \
+        --with-history
+
+    check_file_configuration config.status
+    try $MAKESMP
+    try $MAKESMP install
+
+    if [ ! -e $STAGE_PATH/include/libxml ]; then
+        mk_sym_link $STAGE_PATH/include ./libxml2/libxml libxml
+    fi
+
+    pop_env
+}
+
+# function called after all the compile have been done
+function postbuild_libxml2() {
+    verify_binary lib/$LINK_libxml2
+}
+
+# function to append information to config file
+function add_config_info_libxml2() {
+    append_to_config_file "# libxml2-${VERSION_libxml2}: ${DESC_libxml2}"
+    append_to_config_file "export VERSION_libxml2=${VERSION_libxml2}"
+    append_to_config_file "export LINK_libxml2=${LINK_libxml2}"
+}
---
+++ qgis_deps/recipes/openssl/recipe.sh
@@ -0,0 +1,94 @@
+#!/bin/bash
+
+DESC_openssl="Cryptography and SSL/TLS Toolkit"
+
+# version of your package
+# NOTE openssl version must be compatible with QT version, for example
+# for Qt 5.14 see https://wiki.qt.io/Qt_5.14.1_Known_Issues
+VERSION_openssl=3.4.0
+
+# dependencies of this recipe
+DEPS_openssl=()
+
+LINK_libssl_version=3.4
+LINK_libssl=libssl.${LINK_libssl_version}.dylib
+LINK_libcrypto=libcrypto.${LINK_libssl_version}.dylib
+
+# url of the package
+URL_openssl=https://github.com/openssl/openssl/releases/download/openssl-${VERSION_openssl}/openssl-${VERSION_openssl}.tar.gz
+
+# md5 of the package
+MD5_openssl=34733f7be2d60ecd8bd9ddb796e182af
+
+# default build path
+BUILD_openssl=$BUILD_PATH/openssl/$(get_directory $URL_openssl)
+
+# default recipe path
+RECIPE_openssl=$RECIPES_PATH/openssl
+
+# function called for preparing source code if needed
+# (you can apply patch etc here.)
+function prebuild_openssl() {
+    cd $BUILD_openssl
+
+    # check marker
+    if [ -f .patched ]; then
+        return
+    fi
+
+    patch_configure_file configure
+
+    touch .patched
+}
+
+function shouldbuild_openssl() {
+    # If lib is newer than the sourcecode skip build
+    if [ ${STAGE_PATH}/lib/$LINK_libssl -nt $BUILD_openssl/.patched ]; then
+        DO_BUILD=0
+    fi
+}
+
+# function called to build the source code
+function build_openssl() {
+    try rsync -a $BUILD_openssl/ $BUILD_PATH/openssl/build-$ARCH/
+    try cd $BUILD_PATH/openssl/build-$ARCH
+    push_env
+
+    # This could interfere with how we expect OpenSSL to build.
+    unset OPENSSL_LOCAL_CONFIG_DIR
+
+    # SSLv2 died with 1.1.0, so no-ssl2 no longer required.
+    # SSLv3 & zlib are off by default with 1.1.0 but this may not
+    # be obvious to everyone, so explicitly state it for now to
+    # help debug inevitable breakage.
+    try perl ./Configure \
+        --prefix=$STAGE_PATH \
+        --openssldir=$STAGE_PATH \
+        darwin64-x86_64-cc enable-ec_nistp_64_gcc_128 \
+        no-ssl3 \
+        no-ssl3-method \
+        no-zlib \
+
+    check_file_configuration config.status
+    try $MAKESMP
+    try $MAKESMP install
+
+    pop_env
+}
+
+# function called after all the compile have been done
+function postbuild_openssl() {
+    verify_binary lib/$LINK_libssl
+    verify_binary lib/$LINK_libcrypto
+    verify_binary lib/engines-${LINK_libssl_version}/padlock.dylib
+    verify_binary lib/engines-${LINK_libssl_version}/capi.dylib
+    verify_binary bin/openssl
+}
+
+# function to append information to config file
+function add_config_info_openssl() {
+    append_to_config_file "# openssl-${VERSION_openssl}: ${DESC_openssl}"
+    append_to_config_file "export VERSION_openssl=${VERSION_openssl}"
+    append_to_config_file "export LINK_libssl=${LINK_libssl}"
+    append_to_config_file "export LINK_libcrypto=${LINK_libcrypto}"
+}
