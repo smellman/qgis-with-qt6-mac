d1e8a2700a4a81c4a9a7446f879cee06018b7744
Taro Matsuzawa aka. btm <btm@tech.email.ne.jp>
2025-01-13 16:33:24 +0900

    #5 WIP: added saga recipe, then enable to build (maybe)

---
+++ qgis_deps/recipes/saga/recipe.sh
@@ -0,0 +1,93 @@
+#!/bin/bash
+
+DESC_saga="System for Automated Geoscientific Analyses"
+
+# version of your package
+VERSION_saga=9.7.1
+
+LINK_saga_version=${VERSION_saga}
+
+# dependencies of this recipe
+DEPS_saga=(
+    proj
+    gdal
+    python
+    geos
+    libtiff
+    xz
+    sqlite
+    hdf5
+    netcdf
+    postgres
+    wxmac
+)
+
+# url of the package
+URL_saga=https://downloads.sourceforge.net/project/saga-gis/SAGA%20-%209/SAGA%20-%20${VERSION_saga}/saga-${VERSION_saga}.tar.gz
+
+# md5 of the package
+MD5_saga=74735fcd7ffc2f2c232226305c6a7c1c
+
+# default build path
+BUILD_saga=$BUILD_PATH/saga/$(get_directory $URL_saga)
+
+# default recipe path
+RECIPE_saga=$RECIPES_PATH/saga
+
+# function called for preparing source code if needed
+# (you can apply patch etc here.)
+function prebuild_saga() {
+    cd $BUILD_saga
+
+    # check marker
+    if [ -f .patched ]; then
+        return
+    fi
+
+    # patch_configure_file configure
+
+    touch .patched
+}
+
+function shouldbuild_saga() {
+    # If lib is newer than the sourcecode skip build
+    if [ ${STAGE_PATH}/lib/libsaga_api.dylib -nt $BUILD_saga/.patched ]; then
+        DO_BUILD=0
+    fi
+}
+
+# function called to build the source code
+function build_saga() {
+    try rsync -a $BUILD_saga/ $BUILD_PATH/saga/build-$ARCH/
+    try cd $BUILD_PATH/saga/build-$ARCH
+    push_env
+
+    try ${CONFIGURE} \
+        --disable-debug \
+        --disable-dependency-tracking \
+        --disable-openmp \
+        --disable-libfire \
+        --enable-shared \
+        --disable-gui \
+        --disable-odbc \
+        --with-postgresql=$STAGE_PATH/bin/pg_config
+
+    check_file_configuration config.status
+
+    try $MAKESMP
+    try $MAKESMP install
+
+    pop_env
+}
+
+# function called after all the compile have been done
+function postbuild_saga() {
+    verify_binary "lib/libsaga_api.dylib"
+    verify_binary bin/saga_cmd
+}
+
+# function to append information to config file
+function add_config_info_saga() {
+    append_to_config_file "# saga-${VERSION_saga}: ${DESC_saga}"
+    append_to_config_file "export VERSION_saga=${VERSION_saga}"
+}
