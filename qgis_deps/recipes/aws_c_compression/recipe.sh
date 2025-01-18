729bd78e92f8cb4f58182813829bf4638bb3aaed
Taro Matsuzawa aka. btm <btm@tech.email.ne.jp>
2025-01-18 17:23:45 +0900

    #7 WIP: added some dependencies for aws-sdk-cpp

---
+++ qgis_deps/recipes/aws_c_compression/recipe.sh
@@ -0,0 +1,77 @@
+#!/bin/bash
+
+DESC_aws_c_compression="C99 implementation of huffman encoding/decoding"
+
+# version of your package
+VERSION_aws_c_compression=0.3.0
+LINK_aws_c_compression=libaws-c-compression.dylib
+
+# dependencies of this recipe
+DEPS_aws_c_compression=(aws_c_common)
+
+# url of the package
+URL_aws_c_compression=https://github.com/awslabs/aws-c-compression/archive/refs/tags/v${VERSION_aws_c_compression}.tar.gz
+
+# md5 of the package
+MD5_aws_c_compression=3ecc38992ec676e7927eafd46ce76917
+
+# default build path
+BUILD_aws_c_compression=$BUILD_PATH/aws_c_compression/$(get_directory $URL_aws_c_compression)
+
+# default recipe path
+RECIPE_aws_c_compression=$RECIPES_PATH/aws_c_compression
+
+# function called for preparing source code if needed
+# (you can apply patch etc here.)
+function prebuild_aws_c_compression() {
+    cd $BUILD_aws_c_compression
+
+    # check marker
+    if [ -f .patched ]; then
+        return
+    fi
+
+    touch .patched
+}
+
+function shouldbuild_aws_c_compression() {
+    # If lib is newer than the sourcecode skip build
+    if [ ${STAGE_PATH}/lib/$LINK_aws_c_compression -nt $BUILD_aws_c_compression/.patched ]; then
+        DO_BUILD=0
+    fi
+}
+
+# function called to build the source code
+function build_aws_c_compression() {
+    try mkdir -p $BUILD_PATH/aws_c_compression/build-$ARCH
+    try cd $BUILD_PATH/aws_c_compression/build-$ARCH
+    push_env
+
+    try ${CMAKE} \
+        -DENABLE_TESTING=OFF \
+        $BUILD_aws_c_compression/aws-c-compression
+    check_file_configuration CMakeCache.txt
+
+    try $NINJA
+    try $NINJA install
+
+    # # fixes all libraries install name
+    # for i in `ls ${STAGE_PATH}/lib/libaws*.dylib`;
+    # do
+    #     fix_install_name lib/`basename $i`
+    # done
+
+    pop_env
+}
+
+# function called after all the compile have been done
+function postbuild_aws_c_compression() {
+    verify_binary lib/$LINK_aws_c_compression
+}
+
+# function to append information to config file
+function add_config_info_aws_c_compression() {
+    append_to_config_file "# aws_c_compression-${VERSION_aws_c_compression}: ${DESC_aws_c_compression}"
+    append_to_config_file "export VERSION_aws_c_compression=${VERSION_aws_c_compression}"
+    append_to_config_file "export LINK_aws_c_compression=${LINK_aws_c_compression}"
+}
---
+++ qgis_deps/recipes/aws_c_http/recipe.sh
@@ -0,0 +1,82 @@
+#!/bin/bash
+
+DESC_aws_c_http="C99 implementation of the HTTP/1.1 and HTTP/2 specifications"
+
+# version of your package
+VERSION_aws_c_http=0.9.2
+LINK_aws_c_http=libaws-c-http.dylib
+
+# dependencies of this recipe
+DEPS_aws_c_http=(
+    aws_c_cal
+    aws_c_common
+    aws_c_compression
+    aws_c_io
+)
+
+# url of the package
+URL_aws_c_http=https://github.com/awslabs/aws-c-http/archive/refs/tags/v{VERSION_aws_c_http}.tar.gz
+
+# md5 of the package
+MD5_aws_c_http=5aadae8619a5b09739b99d886075879d
+
+# default build path
+BUILD_aws_c_http=$BUILD_PATH/aws_c_http/$(get_directory $URL_aws_c_http)
+
+# default recipe path
+RECIPE_aws_c_http=$RECIPES_PATH/aws_c_http
+
+# function called for preparing source code if needed
+# (you can apply patch etc here.)
+function prebuild_aws_c_http() {
+    cd $BUILD_aws_c_http
+
+    # check marker
+    if [ -f .patched ]; then
+        return
+    fi
+
+    touch .patched
+}
+
+function shouldbuild_aws_c_http() {
+    # If lib is newer than the sourcecode skip build
+    if [ ${STAGE_PATH}/lib/$LINK_aws_c_http -nt $BUILD_aws_c_http/.patched ]; then
+        DO_BUILD=0
+    fi
+}
+
+# function called to build the source code
+function build_aws_c_http() {
+    try mkdir -p $BUILD_PATH/aws_c_http/build-$ARCH
+    try cd $BUILD_PATH/aws_c_http/build-$ARCH
+    push_env
+
+    try ${CMAKE} \
+        -DENABLE_TESTING=OFF \
+        $BUILD_aws_c_http/aws-c-common
+    check_file_configuration CMakeCache.txt
+
+    try $NINJA
+    try $NINJA install
+
+    # # fixes all libraries install name
+    # for i in `ls ${STAGE_PATH}/lib/libaws*.dylib`;
+    # do
+    #     fix_install_name lib/`basename $i`
+    # done
+
+    pop_env
+}
+
+# function called after all the compile have been done
+function postbuild_aws_c_http() {
+    verify_binary lib/$LINK_aws_c_http
+}
+
+# function to append information to config file
+function add_config_info_aws_c_http() {
+    append_to_config_file "# aws_c_http-${VERSION_aws_c_http}: ${DESC_aws_c_http}"
+    append_to_config_file "export VERSION_aws_c_http=${VERSION_aws_c_http}"
+    append_to_config_file "export LINK_aws_c_http=${LINK_aws_c_http}"
+}
